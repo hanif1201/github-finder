db1695cec6f24fd56df993ca8f7b84d1fb4eac5c
Hanif1201 <adeyemihanif@gmail.com>
2023-10-17 02:38:27 +0100

    env file reated

---
+++ src/components/layout/UserResults/UsersResults.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+
+const UsersResults = () => {
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch(
+      `${import.meta.env.VITE_REACT_APP_GITHUB_URL}/users`,
+      {
+        header: {
+          Authorization: `token ${import.meta.env.VITE_REACT_APP_GITHUB_TOKEN}`,
+        },
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+  };
+
+  return <div>UsersResults</div>;
+};
+
+export default UsersResults;
