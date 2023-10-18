6c00ab2f0c91bb3b601236669d797cec4662949e
Hanif1201 <adeyemihanif@gmail.com>
2023-10-18 01:35:50 +0100

    UserItem Card creation

---
+++ src/components/Users/UsersItem.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+const UsersItem = ({ user: { login, avatar_url } }) => {
+  return (
+    <div className="card shadow-md compact side bg-base-100">
+      <div className="flex-row items-center space-x-4 card-body">
+        <div>
+          <div className="avatar">
+            <div className="rounded-full shadow w-14 h-14">
+              <img src={avatar_url} alt="Profile" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className="card-title">{login}</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+UsersItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
+export default UsersItem;
